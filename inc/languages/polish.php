bfad929af5d7909d20759190185d06329783e2f0
Łukasz <lukasz.orlowski1999@gmail.com>
2020-01-02 00:15:31 +0100

    Aktualizacja daty w komentarzach

--- inc/languages/polish.php
+++ inc/languages/polish.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * MyBB 1.8 Polish Language Pack
- * Copyright © 2008-2019 MyBBoard.pl Team
+ * Copyright © 2008-2020 MyBBoard.pl Team
  * See readme.html for copyright information. | Zapoznaj sie z plikiem czytaj_to.html.
  * Autorzy: bryn, DamYan, gigi, matc, szulcu, Łukasz, Divir | Testerzy: hippie
  */
