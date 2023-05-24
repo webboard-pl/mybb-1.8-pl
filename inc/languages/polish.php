3d13b37a7b32403d666a93ba760675522a63e81c
Łukasz <19271038+Lukaszo99@users.noreply.github.com>
2023-05-24 22:57:34 +0200

    Update to MyBB 1.8.34
    Aktualizacja doi MyBB 1.8.34

--- inc/languages/polish.php
+++ inc/languages/polish.php
@@ -28,7 +28,7 @@ $langinfo['website'] = "https://webboard.pl/";
 //$langinfo['additional_name'] = " ";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1833";
+$langinfo['version'] = "1834";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
